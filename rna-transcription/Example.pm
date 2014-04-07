82a00c76423878e86e66068ba100d99b526830c9
Eman <kungperl@gmail.com>
2014-04-07 13:58:22 +0800

    update the perl test and Example code to match README

--- rna-transcription/Example.pm
+++ rna-transcription/Example.pm
@@ -4,7 +4,7 @@ use warnings;
 
 sub to_rna {
 	my ($dna) = @_;
-    $dna =~ y/T/U/;
+    $dna =~ tr/AGCT/UCGA/;
     #$dna =~ tr/T/U/;   # y and tr are synonymes in perl 5
     #$dna =~ s/T/U/g;   # regexes are not really the right tool for this
 	return $dna;
