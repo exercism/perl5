08ffd1e469c0abf9887cd1b13987c9111a3d6667
Erick <kungperl@gmail.com>
2014-05-26 15:35:23 +0800

    Add sum-of-multiples exercise

---
+++ sum-of-multiples/Example.pm
@@ -0,0 +1,32 @@
+package Example;
+
+use strict;
+use warnings;
+use List::Util 'sum';
+
+sub new {
+    my ($class, @multiples_of) = @_;
+    @multiples_of = (3, 5) unless @multiples_of;
+
+    bless { multiples_of => [@multiples_of] } => $class;
+}
+
+sub to {
+    my ($self, $to) = @_;
+    $to = $to - 1;
+    my $sum;
+    my @multiples;
+    
+    foreach my $num (1..$to) {
+        foreach my $multiple_of ( @{ $self->{multiples_of} } ) {
+            if ($num % $multiple_of == 0) {
+                push @multiples => $num;
+                last;
+            }
+        }
+    }
+
+    return (sum @multiples) || 0;
+}
+
+1;
