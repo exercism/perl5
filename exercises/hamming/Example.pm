e3590effc83628f096d7adade1a10057712b2d15
Yanick Champoux <yanick@babyl.dyndns.org>
2016-12-05 17:55:29 -0500

    change requirement for different lengths
    Now we must die if the strings are of different lengths
    
    Fixes #81

--- exercises/hamming/Example.pm
+++ exercises/hamming/Example.pm
@@ -3,10 +3,17 @@ package Example;
 use strict;
 use warnings;
 
+use Carp;
+
 sub compute {
     my ($strand_a, $strand_b) = @_;
+
+    croak "DNA strands must be of equal length"
+        unless length $strand_a == length $strand_b;
+
     my $distance = $strand_a ^ $strand_b;
     $distance =~ s/\0//g;
+
     return (length $distance) - abs(length($strand_a) - length($strand_b));
 }
 
