85a6aa1b619d2e2326c2f14f448a5f02e4dd7f5e
Daniel Mita <mienaikage@gmail.com>
2017-06-29 19:02:47 +0100

    Generate leap

--- exercises/leap/Example.pm
+++ exercises/leap/Example.pm
@@ -1,16 +1,17 @@
-package Example;
-use warnings;
+# Declare package 'Leap' with version
+package Leap 2;
 use strict;
+use warnings;
 
 sub is_leap {
-    my $year = shift;
-    divisible_by($year, 400)
-        or divisible_by($year, 4) and !divisible_by($year, 100)
-        ? 1 : 0;
+  my $year = shift;
+  divisible_by($year, 400)
+    or divisible_by($year, 4) and !divisible_by($year, 100)
+    ? 1 : 0;
 }
 
 sub divisible_by {
-    $_[0] % $_[1] == 0 ? 1 : 0;
+  $_[0] % $_[1] == 0 ? 1 : 0;
 }
 
-__PACKAGE__;
+1;
---
+++ exercises/leap/Leap.pm
@@ -0,0 +1,11 @@
+# Declare package 'Leap' with version
+package Leap 2;
+use strict;
+use warnings;
+
+sub is_leap {
+  my ($year) = @_;
+  # Remove the comments and write some code here to pass the test suite.
+}
+
+1;
