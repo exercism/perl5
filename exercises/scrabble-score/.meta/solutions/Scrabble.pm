3ab88202e67764048db795874547fdb019e7896c
Simon Shine <shreddedglory@gmail.com>
2018-12-07 09:18:43 +0100

    Scrabble: Revise example solution based on feedback

--- exercises/scrabble-score/.meta/solutions/Scrabble.pm
+++ exercises/scrabble-score/.meta/solutions/Scrabble.pm
@@ -1,30 +1,31 @@
 package Scrabble;
 use strict;
 use warnings;
-use experimental 'signatures';
-use List::Util qw(sum0);
 use Exporter 'import';
 our @EXPORT_OK = qw(score);
 
-sub points ($points, @letters) {
-    map { $_ => $points } @letters;
-}
+use experimental 'signatures';
+use List::Util qw(sum0 pairmap);
 
-my %scores = (
-    points(1, qw(A E I O U L N R S T)),
-    points(2, qw(D G)),
-    points(3, qw(B C M P)),
-    points(4, qw(F H V W Y)),
-    points(5, qw(K)),
-    points(8, qw(J X)),
-    points(10, qw(Q Z)),
-);
+my $scores = {
+    pairmap { map { $_ => $a } split //, $b } qw(
+        1 aeioulnrst
+        2 dg
+        3 bcmp
+        4 fhvwy
+        5 k
+        8 jx
+        10 qz
+    )
+};
 
 sub score ($word, %extensions) {
+    my $double = $extensions{double} // 0;
+    my $triple = $extensions{triple} // 0;
     return sum0 map {
-        $scores{uc $_} * 2 ** ($extensions{double} // 0)
-                       * 3 ** ($extensions{triple} // 0)
-    } $word =~ m/([A-Z])/ig;
+        $scores->{$_} * 2 ** $double
+                      * 3 ** $triple
+    } split //, lc $word;
 }
 
 1;
