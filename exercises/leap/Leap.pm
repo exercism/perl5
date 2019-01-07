66f737da0ab46f7947ee01973dee1cb38ec6f592
Daniel Mita <mienaikage@gmail.com>
2019-01-07 01:44:38 +0000

    Check for definedness in leap tests

--- exercises/leap/Leap.pm
+++ exercises/leap/Leap.pm
@@ -3,10 +3,11 @@ package Leap;
 use strict;
 use warnings;
 use Exporter 'import';
-our @EXPORT_OK = qw(is_leap);
+our @EXPORT_OK = qw(is_leap_year);
 
-sub is_leap {
+sub is_leap_year {
   my ($year) = @_;
+  return undef;
   # Remove the comments and write some code here to pass the test suite.
 }
 
