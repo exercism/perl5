b4fa07dd699f13e9c2f43c84c28195bd83e0340e
Simon Shine <shreddedglory@gmail.com>
2019-04-17 10:05:55 +0200

    custom-set: Add exercise stub
    To make it more clear what exercises are to be implemented, and what
    arguments they take, this exercise stub lists the methods with their
    expected arguments and dies with a standard error message.
    
    The constructor is listed first.
    
    Then the two mutable methods `add` and `remove` to distinguish them.
    
    Then the simplest immutable methods, so `is_empty`, `is_member`, `size`
    and `to_list`.
    
    Then `union`, `intersect` and `difference` are listed: If written first
    they can be used to define the remaining functions, so encourage the
    student to write them first. Listed in order of simplicity.
    
    Lastly `is_disjoint`, `is_subset` and `is_equal` are listed as the most
    difficult, made simpler if the previous methods were already written.
    Also listed in order of simplicity.

---
+++ exercises/custom-set/CustomSet.pm
@@ -0,0 +1,77 @@
+package CustomSet;
+use strict;
+use warnings;
+use Exporter 'import';
+our @EXPORT_OK = qw(
+    new add remove is_empty
+    is_member size to_list
+    union intersect difference
+    is_disjoint is_equal is_subset
+);
+
+sub new {
+    my ($class, @members) = @_;
+    die "You need to implement this.";
+}
+
+sub add {
+    my ($self, $member) = @_;
+    die "You need to implement this.";
+}
+
+sub remove {
+    my ($self, $member) = @_;
+    die "You need to implement this.";
+}
+
+sub is_empty {
+    my ($self) = @_;
+    die "You need to implement this.";
+}
+
+sub is_member {
+    my ($self, $member) = @_;
+    die "You need to implement this.";
+}
+
+sub size {
+    my ($self) = @_;
+    die "You need to implement this.";
+}
+
+sub to_list {
+    my ($self) = @_;
+    die "You need to implement this.";
+}
+
+sub union {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+sub intersect {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+sub difference {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+sub is_disjoint {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+sub is_subset {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+sub is_equal {
+    my ($self, $other) = @_;
+    die "You need to implement this.";
+}
+
+1;
