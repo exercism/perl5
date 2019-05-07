0cc4429b9e7ef675bc60c162b224d2d4e9e420fc
Simon Shine <shreddedglory@gmail.com>
2019-05-07 08:38:59 +0200

    custom-set: Replace `die "..."` with `...`
    https://perldoc.pl/perlsyn#The-Ellipsis-Statement

--- exercises/custom-set/CustomSet.pm
+++ exercises/custom-set/CustomSet.pm
@@ -10,67 +10,67 @@ our @EXPORT_OK = qw(
 
 sub new {
     my ($class, @members) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub add {
     my ($self, $member) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub remove {
     my ($self, $member) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub is_empty {
     my ($self) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub is_member {
     my ($self, $member) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub size {
     my ($self) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub to_list {
     my ($self) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub union {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub intersect {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub difference {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub is_disjoint {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub is_subset {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 sub is_equal {
     my ($self, $other) = @_;
-    die "You need to implement this.";
+    ...;
 }
 
 1;
