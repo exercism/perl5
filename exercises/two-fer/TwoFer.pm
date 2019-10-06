29305dc1255b993e2b0df1a5e50fa10942740e7a
Daniel Mita <mienaikage@users.noreply.github.com>
2019-10-06 11:27:30 +0100

    Update two-fer to suggest the use of signatures (#330)

--- exercises/two-fer/TwoFer.pm
+++ exercises/two-fer/TwoFer.pm
@@ -10,3 +10,14 @@ sub two_fer {
 }
 
 1;
+
+__END__
+If you are using perl v5.20 or newer, you
+can replace the existing sub declaration
+with the following code:
+
+use experimental qw(signatures);
+
+sub two_fer ($name = undef) {}
+
+1;
