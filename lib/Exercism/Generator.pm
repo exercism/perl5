955058600548c4be7e58b0b261c65c3ca74e8fde
Daniel Mita <mienaikage@users.noreply.github.com>
2020-10-15 17:34:45 +0100

    Update generator to parse tests from toml files (#349)
    * Update generator to parse tests from toml files
    
    * Replace TOML::Tiny with TOML::Parser
    
    * Skip generator comparison tests for older perls

--- lib/Exercism/Generator.pm
+++ lib/Exercism/Generator.pm
@@ -2,40 +2,64 @@ package Exercism::Generator;
 
 use Moo;
 
-use Path::Tiny;
-use Perl::Tidy;
-use Template::Mustache qw(render);
+use JSON::PP ();
+use List::Util qw<any>;
+use Path::Tiny qw<path>;
+use Perl::Tidy ();
+use Template::Mustache qw<render>;
+use TOML::Parser ();
+
+use namespace::clean;
 
 use constant BASE_DIR =>
   path(__FILE__)->realpath->parent->parent->parent;
-use Exporter qw(import);
-our @EXPORT_OK = qw(BASE_DIR);
+use Exporter qw<import>;
+our @EXPORT_OK = qw<BASE_DIR>;
 
-has [qw(data exercise)] => (
+has exercise => (
   is       => 'ro',
   required => 1,
 );
 
-has [qw(cdata ignore_cdata)] => ( is => 'lazy' );
+has data => (
+  is       => 'ro',
+  required => 1,
+  default => sub { {} },
+);
 
+has json => (
+  is       => 'ro',
+  required => 1,
+  default  => sub { JSON::PP->new->canonical->pretty },
+);
+
+has [
+  qw<
+    case_uuids
+    cases
+    cdata
+    json_tests
+    >
+] => ( is => 'lazy' );
+
+# test returns the test file rendered from the template
 sub test {
   $_[0]->_render( { template => 'test' } );
 }
 
+# stub returns the stub module file rendered from the template
 sub stub {
   $_[0]->_render( { template => 'module', file => 'stub' } );
 }
 
+# example returns the example module file rendered from the template
 sub example {
   $_[0]->_render( { template => 'module', file => 'example' } );
 }
 
 sub _render {
   my ( $self, $params ) = @_;
-  my $data = { %{ $self->data } };
-  if ( $self->cdata && !$self->ignore_cdata ) {
-    $data->{cdata}{json} = $self->cdata;
-  }
+  my $data = { %{ $self->data }, cases => $self->json_tests };
 
   if ( $params->{file} ) {
     $data->{module_file} = $data->{ $params->{file} };
@@ -55,20 +79,73 @@ sub _render {
   return $tidied;
 }
 
-sub _build_ignore_cdata {
-  $_[0]->data->{ignore_cdata};
+# case_uuids contains an array of UUIDs marked as true in tests.toml
+sub _build_case_uuids {
+  my ($self) = @_;
+
+  my $toml_file = BASE_DIR->child(
+    'exercises/' . $self->exercise . '/.meta/tests.toml' );
+
+  my $toml_data;
+  if ( $toml_file->is_file ) {
+    $toml_data = TOML::Parser->new->parse( $toml_file->slurp );
+    return [
+      grep { $toml_data->{'canonical-tests'}{$_} }
+        keys %{ $toml_data->{'canonical-tests'} }
+    ];
+  }
+
+  return [];
 }
 
+# cases is an array of the cases matching case_uuids
+sub _build_cases {
+  my ( $self, $obj, $description ) = @_;
+  return [] if !$self->cdata;
+
+  $obj //= $self->json->decode( $self->cdata );
+  my $new_desc = '';
+
+  if ( $obj->{cases} ) {
+    $new_desc = ( $description // '' ) . $obj->{description} . ': '
+      if $obj->{description};
+
+    return [
+      map {
+        @{ $self->_build_cases( $_, $new_desc || $description ) }
+      } @{ $obj->{cases} }
+    ];
+  }
+  elsif ( any { $_ eq $obj->{uuid} } @{ $self->case_uuids } ) {
+    return [
+      { %{$obj}{qw<input expected property>},
+        description => ( $description // '' ) . $obj->{description}
+      }
+    ];
+  }
+
+  return [];
+}
+
+# cdata is the canonical-data.json for an exercise in problem-specifications
 sub _build_cdata {
   my ($self) = @_;
   my $cdata_file
     = BASE_DIR->child( 'problem-specifications/exercises/'
       . $self->exercise
       . '/canonical-data.json' );
+
   if ( $cdata_file->is_file ) {
     return $cdata_file->slurp =~ s/^\s+|\s+$//gr;
   }
-  return;
+
+  return '';
+}
+
+# json_tests is cases transformed into a JSON array
+sub _build_json_tests {
+  my ($self) = @_;
+  return @{ $self->cases } ? $self->json->encode( $self->cases ) : '';
 }
 
 1;
