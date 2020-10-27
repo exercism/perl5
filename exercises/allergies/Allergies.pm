8556550ee13e88968c420d452015640509964f98
Daniel Mita <mienaikage@users.noreply.github.com>
2020-10-27 01:26:04 +0000

    Add allergies to generator (#264)

---
+++ exercises/allergies/Allergies.pm
@@ -0,0 +1,17 @@
+package Allergies;
+use strict;
+use warnings;
+use Exporter qw<import>;
+our @EXPORT_OK = qw<allergic_to list_allergies>;
+
+sub allergic_to {
+  my ($input) = @_;
+  return undef;
+}
+
+sub list_allergies {
+  my ($score) = @_;
+  return undef;
+}
+
+1;
