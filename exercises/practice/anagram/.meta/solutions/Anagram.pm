0f6ce1ba5e00e5344e4100a40ed0956fb1bcb1e2
Daniel Mita <noreply@dango.space>
2021-04-02 23:12:48 +0100

    Add anagram solution

--- exercises/practice/anagram/.meta/solutions/Anagram.pm
+++ exercises/practice/anagram/.meta/solutions/Anagram.pm
@@ -6,7 +6,14 @@ our @EXPORT_OK = qw<match_anagrams>;
 
 sub match_anagrams {
   my ($input) = @_;
-  return [];
+
+  return [
+    grep {
+      lc $_ ne lc $input->{subject}
+        && join( '', sort( split( //, lc $_ ) ) ) eq
+        join( '', sort( split( //, lc $input->{subject} ) ) )
+    } @{ $input->{candidates} }
+  ];
 }
 
 1;
