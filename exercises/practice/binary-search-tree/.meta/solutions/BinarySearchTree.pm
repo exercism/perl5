df88b3e453c461c3c40c5fe651d391eda0bec376
Glenn Jackman <jackman@pythian.com>
2021-09-30 16:00:35 -0400

    This adds subtests for the `sortedData` test cases. Also adds an example solution to the exercise's yaml data.
    Resolves #433

--- exercises/practice/binary-search-tree/.meta/solutions/BinarySearchTree.pm
+++ exercises/practice/binary-search-tree/.meta/solutions/BinarySearchTree.pm
@@ -2,18 +2,53 @@ package BinarySearchTree;
 use strict;
 use warnings;
 use Exporter qw<import>;
-our @EXPORT_OK = qw<tree>;
-
-use lib 'lib';
-use Exercism::QuickSolve;
+our @EXPORT_OK = qw<tree treeSort>;
 
 sub tree {
   my ($data) = @_;
+  my $tree = leaf();
+  insert( $tree, $_ ) for @$data;
+  return $tree;
+}
 
-  quicksolve(
-    input     => $data,
-    input_key => 'treeData',
-  );
+sub leaf {
+  return {
+    data  => undef,
+    left  => undef,
+    right => undef,
+  };
+}
+
+sub insert {
+  my ( $tree, $value ) = @_;
+  if ( not defined $tree->{data} ) {
+    $tree->{data} = $value;
+  }
+  elsif ( $value <= $tree->{data} ) {
+    $tree->{left} //= leaf();
+    insert( $tree->{left}, $value );
+  }
+  else {
+    $tree->{right} //= leaf();
+    insert( $tree->{right}, $value );
+  }
+  return;
+}
+
+sub treeSort {
+  my ($data) = @_;
+  my $tree   = tree($data);
+  my @values = ();
+  walk( $tree, sub { push @values, (shift)->{data} } );
+  return \@values;
+}
+
+sub walk {
+  my ( $tree, $sub ) = @_;
+  walk( $tree->{left}, $sub ) if defined $tree->{left};
+  $sub->($tree);
+  walk( $tree->{right}, $sub ) if defined $tree->{right};
+  return;
 }
 
 1;
