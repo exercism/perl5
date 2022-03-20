e42f10de926ec97d0ee920745118f6c1ddbc12e1
Daniel Mita <noreply@dango.space>
2022-03-20 01:06:13 +0000

    Add real solution to all-your-base

--- exercises/practice/all-your-base/AllYourBase.pm
+++ exercises/practice/all-your-base/AllYourBase.pm
@@ -4,6 +4,12 @@ use warnings;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<rebase>;
 
+my @errors = (
+  'input base must be >= 2',
+  'output base must be >= 2',
+  'all digits must satisfy 0 <= d < input base',
+);
+
 sub rebase {
   my ($input) = @_;
   return undef;
