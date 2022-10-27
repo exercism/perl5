941bea09234097f06a1ea0948cde766db415e46b
Daniel Mita <noreply@dango.space>
2022-10-27 20:55:52 +0100

    Generate full Perl tests for crypto-square

--- exercises/practice/crypto-square/.meta/solutions/CryptoSquare.pm
+++ exercises/practice/crypto-square/.meta/solutions/CryptoSquare.pm
@@ -7,16 +7,38 @@ use feature qw<say>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<cipher>;
 
-use lib 'lib';
-use Exercism::QuickSolve;
-
 sub cipher {
     my ($text) = @_;
 
-    quicksolve(
-        input     => $text,
-        input_key => 'plaintext',
-    );
+    $text = lc($text);
+    $text =~ s/[^a-z0-9]//g;
+
+    my ( $cols, $rows ) = ( 1, 0 );
+    until ( $cols * $rows >= length($text) ) {
+        if ( $cols > $rows ) {
+            $rows++;
+        }
+        else {
+            $cols++;
+        }
+    }
+
+    $text .= ' ' x ( $cols * $rows - length($text) );
+
+    my @matrix;
+    for ( my $i = 0; $i < $cols * $rows; $i += $cols ) {
+        push @matrix, [ split //, substr( $text, $i, $cols ) ];
+    }
+
+    my $result = '';
+    for my $j ( 0 .. $cols - 1 ) {
+        $result .= ' ' if $result;
+        for my $k ( 0 .. $rows - 1 ) {
+            $result .= $matrix[$k][$j];
+        }
+    }
+
+    return $result;
 }
 
 1;
