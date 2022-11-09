db14a894a2f15a4e7779dcfdeb8167d315e8a67e
Daniel Mita <noreply@dango.space>
2022-11-09 12:47:45 +0000

    Generate full Perl tests for secret-handshake

--- exercises/practice/secret-handshake/.meta/solutions/SecretHandshake.pm
+++ exercises/practice/secret-handshake/.meta/solutions/SecretHandshake.pm
@@ -7,16 +7,15 @@ use feature qw<say>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<handshake>;
 
-use lib 'lib';
-use Exercism::QuickSolve;
-
 sub handshake {
     my ($number) = @_;
-
-    quicksolve(
-        input     => $number,
-        input_key => 'number',
-    );
+    my @result;
+    push @result, 'wink'            if 0b00001 & $number;
+    push @result, 'double blink'    if 0b00010 & $number;
+    push @result, 'close your eyes' if 0b00100 & $number;
+    push @result, 'jump'            if 0b01000 & $number;
+    @result = reverse @result if 0b10000 & $number;
+    return \@result;
 }
 
 1;
