262eaa55fe70c4de4c346d46f22cbda0447a3aa9
Daniel Mita <noreply@dango.space>
2023-03-31 22:54:42 +0100

    Add error checks

--- exercises/practice/queen-attack/.meta/solutions/Queen.pm
+++ exercises/practice/queen-attack/.meta/solutions/Queen.pm
@@ -4,11 +4,19 @@ use Moo;
 use feature qw<say>;
 
 has row => (
-    is => 'ro',
+    is  => 'ro',
+    isa => sub {
+        die 'row not positive' if $_[0] < 0;
+        die 'row not on board' if $_[0] > 7;
+    },
 );
 
 has column => (
-    is => 'ro',
+    is  => 'ro',
+    isa => sub {
+        die 'column not positive' if $_[0] < 0;
+        die 'column not on board' if $_[0] > 7;
+    },
 );
 
 sub can_attack {
