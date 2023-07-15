86f3a3f7bb66d4e8ae43538be148530d3ada09b0
Daniel Mita <noreply@dango.space>
2023-07-16 00:28:50 +0100

    Generate full Perl tests for space-age

--- exercises/practice/space-age/.meta/solutions/SpaceAge.pm
+++ exercises/practice/space-age/.meta/solutions/SpaceAge.pm
@@ -8,13 +8,25 @@ use experimental qw<signatures postderef postderef_qq>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<age_on_planet>;
 
-use lib 'lib';
-use Exercism::QuickSolve;
+# Solved by GitHub Copilot
+sub age_on_planet ( $planet, $seconds ) {
+    my %orbital_periods = (
+        Mercury => 0.2408467,
+        Venus   => 0.61519726,
+        Earth   => 1.0,
+        Mars    => 1.8808158,
+        Jupiter => 11.862615,
+        Saturn  => 29.447498,
+        Uranus  => 84.016846,
+        Neptune => 164.79132,
+    );
 
-sub age_on_planet {
-    my ($args) = @_;
+    die "not a planet" unless exists $orbital_periods{$planet};
 
-    quicksolve( input => $args );
+    my $earth_years   = $seconds / 31557600;
+    my $age_on_planet = $earth_years / $orbital_periods{$planet};
+
+    return sprintf "%.2f", $age_on_planet;
 }
 
 1;
