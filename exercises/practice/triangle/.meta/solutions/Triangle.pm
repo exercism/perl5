43f75c3756cbc8bdd09afedcb55ef9a8a7fcca83
Daniel Mita <noreply@dango.space>
2023-07-16 12:53:44 +0100

    Generate full Perl tests for triangle

--- exercises/practice/triangle/.meta/solutions/Triangle.pm
+++ exercises/practice/triangle/.meta/solutions/Triangle.pm
@@ -8,32 +8,30 @@ use experimental qw<signatures postderef postderef_qq>;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<is_equilateral is_isosceles is_scalene>;
 
-use experimental qw<signatures>;
-use lib 'lib';
-use Exercism::QuickSolve;
-
-sub is_equilateral ($triangle) {
-    !!quicksolve(
-        input     => $triangle,
-        input_key => 'sides',
-        property  => 'equilateral',
-    );
+# Solution by GitHub Copilot
+sub is_equilateral ($sides) {
+    my $first_side = $sides->[0];
+    for my $side (@$sides) {
+        return 0 if $side == 0;
+        return 0 if $side != $first_side;
+    }
+    return 1;
 }
 
-sub is_isosceles ($triangle) {
-    !!quicksolve(
-        input     => $triangle,
-        input_key => 'sides',
-        property  => 'isosceles',
-    );
+sub is_isosceles ($sides) {
+    my ( $first_side, $second_side, $third_side )
+        = sort { $a <=> $b } @$sides;
+    return 0 if $first_side + $second_side <= $third_side;
+    return 1 if $first_side == $second_side || $second_side == $third_side;
+    return 0;
 }
 
-sub is_scalene ($triangle) {
-    !!quicksolve(
-        input     => $triangle,
-        input_key => 'sides',
-        property  => 'scalene',
-    );
+sub is_scalene ($sides) {
+    my ( $first_side, $second_side, $third_side )
+        = sort { $a <=> $b } @$sides;
+    return 0 if $first_side + $second_side <= $third_side;
+    return 1 if $first_side != $second_side && $second_side != $third_side;
+    return 0;
 }
 
 1;
