53f00cca11df080d954a7e039399b4ff31ff3478
Glenn Jackman <jackman@pythian.com>
2024-11-11 14:38:02 -0500

    Add collatz-conjecture exercise

---
+++ exercises/practice/collatz-conjecture/.meta/solutions/t/collatz-conjecture.t
@@ -0,0 +1,1 @@
+../../../t/collatz-conjecture.t
---
+++ exercises/practice/collatz-conjecture/lib/CollatzConjecture.pm
@@ -0,0 +1,12 @@
+package CollatzConjecture;
+
+use v5.40;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<steps>;
+
+sub steps ($number) {
+    return undef;
+}
+
+1;
