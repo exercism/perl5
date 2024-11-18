a3e838379ce7933e69d988b564032fb812d2bb51
Daniel Mita <noreply@dango.space>
2024-11-18 18:38:20 +0000

    snake_case variables

--- exercises/practice/two-bucket/lib/TwoBucket.pm
+++ exercises/practice/two-bucket/lib/TwoBucket.pm
@@ -5,7 +5,7 @@ use v5.40;
 use Exporter qw<import>;
 our @EXPORT_OK = qw<measure>;
 
-sub measure ( $bucketOne, $bucketTwo, $goal, $startBucket ) {
+sub measure ( $bucket_1, $bucket_2, $goal, $start_bucket ) {
     return undef;
 }
 
