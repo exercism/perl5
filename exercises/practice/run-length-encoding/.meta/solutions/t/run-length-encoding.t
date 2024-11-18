b53dd1b98c6ac741abe46de224fef97fe6d44c25
Glenn Jackman <glenn.jackman@gmail.com>
2024-11-18 13:42:36 -0500

    Add run-length-encoding exercise (#693)

---
+++ exercises/practice/run-length-encoding/.meta/solutions/t/run-length-encoding.t
@@ -0,0 +1,1 @@
+../../../t/run-length-encoding.t
---
+++ exercises/practice/run-length-encoding/lib/RunLengthEncoding.pm
@@ -0,0 +1,16 @@
+package RunLengthEncoding;
+
+use v5.40;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<encode decode>;
+
+sub encode ($string) {
+    return undef;
+}
+
+sub decode ($string) {
+    return undef;
+}
+
+1;
