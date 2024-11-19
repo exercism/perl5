6e3c6b3b3cc3ba6acbb3e22a267cf9a9f1b44288
Glenn Jackman <glenn.jackman@gmail.com>
2024-11-19 01:31:03 -0500

    Add two-bucket exercise (#692)
    * Add two-bucket exercise
    
    ---------
    
    Co-authored-by: Daniel Mita <noreply@dango.space>

---
+++ exercises/practice/two-bucket/lib/TwoBucket.pm
@@ -0,0 +1,12 @@
+package TwoBucket;
+
+use v5.40;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<measure>;
+
+sub measure ( $bucket_1, $bucket_2, $goal, $start_bucket ) {
+    return undef;
+}
+
+1;
